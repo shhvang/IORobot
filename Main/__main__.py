6dc5b26a02201a13dc1467fd1668b33d435a82bf
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-22 17:35:42 +0530

    Update __main__.py

--- Main/__main__.py
+++ Main/__main__.py
@@ -4,8 +4,8 @@ from telegram.ext import ContextTypes, CommandHandler
 
 from Main.utils.decorators import kiyocmd
 
-import logging
-from . import application
+import time
+from . import application, LOGS, start_time
 
 txt = '''
 Hey *{}*!, 
@@ -19,7 +19,17 @@ Click Help button to find out more about how to use me to my full potential.
 If you have any questions, need assistance with something, or just want to have a friendly chat, don't hesitate to reach out.
 '''
 
-@kiyocmd('start')
+def get_readable_time(seconds: int) -> str:
+    intervals = [('days', 86400), ('h', 3600), ('m', 60), ('s', 1)]
+    time_string = ''
+    for name, count in intervals:
+        value = seconds // count
+        if value > 0:
+            seconds -= value * count
+            time_string += f'{value}{name}'
+
+    return time_string
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.type == "private":
         await update.effective_message.reply_video(
@@ -28,12 +38,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             parse_mode=ParseMode.MARKDOWN
         )
     else:
-        await update.effective_message.reply_text(f"Hi, I'm {context.bot.first_name}")
+        await update.effective_message.reply_text(f"Hi, I'm {context.bot.first_name}\nuptime: {get_readable_time(time.time() - start_time)}")
 
 def main():
-    logging.info('Successfully started!')
+    LOGS.info('Successfully started!')
+    application.add_handler(CommandHandler('start', start))
     application.run_polling()
-    
 
 if __name__ == '__main__':
     main()
