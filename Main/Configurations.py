bcb91547525d3182ca48aae7a0d2cff6a3e55a37
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-22 12:45:30 +0530

    Update Configurations.py

--- Main/Configurations.py
+++ Main/Configurations.py
@@ -1,3 +1,3 @@
-Token = '1601619815:AAHHCDk-6nRr0ef5ApBd1oiGVXPrWdZycQY'
+Token = '7042927041:AAH0QumB94lyhjfN_fTvbkVw0zQ9HL7INqY'
 BaseUrl = 'https://api.telegram.org/bot'
 BaseFileUrl = 'https://api.telegram.org/file/bot'
