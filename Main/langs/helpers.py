2bcc9ef0254bdab3c04e01ea2a3a5b9beb191c46
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-22 21:29:25 +0530

    Update helpers.py

--- Main/langs/helpers.py
+++ Main/langs/helpers.py
@@ -5,42 +5,39 @@ import yaml
 import functools
 from pathlib import Path
 
-class Language:
-    def __init__(self):
-        self.lang_strings_cache = {}
-        self.languages = {}
-        self.setup_localization()
+lang_strings_cache = {}
+languages = {}
 
-    def get_string(self, lang: str, key: str) -> str:
-        lang_strings = self.lang_strings_cache.get(lang)
-        if lang_strings is None:
-            lang_strings = self.reload_strings(lang)
-            self.lang_strings_cache[lang] = lang_strings
-        return lang_strings.get(key, f"Missing translation for key '{key}'")
+@functools.lru_cache(maxsize=None)
+def reload_strings(lang_code: str = "en") -> Dict[str, str]:
+    lang_file = Path("locales") / f"{lang_code}.yaml"
+    if lang_file.exists():
+        with lang_file.open("r", encoding="utf-8") as file:
+            lang_data = yaml.safe_load(file)
+            languages[lang_code] = lang_data  # Store in languages
+            return lang_data
+    else:
+        logging.info(f"Localization file not found for '{lang_code}'")
+        return {}
 
-    @functools.lru_cache(maxsize=None)
-    def reload_strings(self, lang_code: str = "en") -> Dict[str, str]:
-        lang_file = Path("locales") / f"{lang_code}.yaml"
-        if lang_file.exists():
-            with lang_file.open("r", encoding="utf-8") as file:
-                lang_data = yaml.safe_load(file)
-                self.languages[lang_code] = lang_data  # Store in self.languages
-                return lang_data
-        else:
-            logging.info(f"Localization file not found for '{lang_code}'")
-            return {}
+def get_string(lang: str, key: str) -> str:
+    lang_strings = lang_strings_cache.get(lang)
+    if lang_strings is None:
+        lang_strings = reload_strings(lang)
+        lang_strings_cache[lang] = lang_strings
+    return lang_strings.get(key, f"Missing translation for key '{key}'")
 
-    def get_languages(self) -> Dict[str, str]:
-        return {lang: data["language"] for lang, data in self.languages.items()}
+def get_languages() -> Dict[str, str]:
+    return {lang: data["language"] for lang, data in languages.items()}
 
-    def get_language(self, language: str) -> str:
-        return self.languages[language]["language"]
+def get_language(language: str) -> str:
+    return languages[language]["language"]
 
-    def setup_localization(self) -> None:
-        lang_files = os.listdir("locales")
-        if not lang_files:
-            logging.info("Localization files not found!")
-            logging.info("Using the default language - English.")
-            self.reload_strings() 
-        else:
-            logging.info("Localization setup complete!")
+def setup_localization() -> None:
+    lang_files = os.listdir("locales")
+    if not lang_files:
+        logging.info("Localization files not found!")
+        logging.info("Using the default language - English.")
+        reload_strings() 
+    else:
+        logging.info("Localization setup complete!")
