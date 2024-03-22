16cbd03d41340527c3b93cd37b7d9293840343dc
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-22 21:29:08 +0530

    Update __init__.py

--- Main/langs/__init__.py
+++ Main/langs/__init__.py
@@ -1,1 +1,18 @@
+from typing import Union
+import Main.sql.language_sql as sql
+from Main.langs.helpers import (
+    get_language,
+    get_languages,
+    get_string,
+    setup_localization
+)
 
+def tlang(
+    chat_id: Union[int, str], 
+    string: str
+) -> str:
+    lang = sql.get_chat_lang(chat_id)
+    return get_string(lang, string)
+
+def useless_func() -> str:
+    return tlang, get_string
