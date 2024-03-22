c2d517ef50bfd63830b59dd50c229cfaf2b25d4d
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-22 15:32:29 +0530

    Update Configurations.py

--- Main/Configurations.py
+++ Main/Configurations.py
@@ -1,3 +1,6 @@
 Token = '7042927041:AAH0QumB94lyhjfN_fTvbkVw0zQ9HL7INqY'
 BaseUrl = 'https://api.telegram.org/bot'
 BaseFileUrl = 'https://api.telegram.org/file/bot'
+DEV_ID = [1, 2, 3]
+SUDO_ID = [1, 2, 3]
+OWNER_ID = [1]
