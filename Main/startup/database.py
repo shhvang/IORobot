38d90095e789b510d719d7d9a2020c073a44dfe3
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-22 23:22:28 +0530

    Update database.py

--- Main/startup/database.py
+++ Main/startup/database.py
@@ -1,42 +1,39 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.pool import QueuePool
 
 class Database:
     def __init__(self, db_uri: str, db_type: str = 'postgresql', debug: bool = False,
-                 base_cls=None, pool_size: int = 5, max_overflow: int = 10, scopefunc=None):
-        self.db_uri = db_uri
-        self.db_type = db_type
-        self.debug = debug
-        self.base = base_cls or declarative_base()
+                 pool_size: int = 5, max_overflow: int = 10, scopefunc=None):
+        self.metadata = MetaData()
+        self.Base = declarative_base(metadata=self.metadata)
+        self.Session = None
         self.engine = None
-        self.session = None
-        self.pool_size = pool_size
-        self.max_overflow = max_overflow
-        self.scopefunc = scopefunc
+        self.connect(db_uri, db_type, debug, pool_size, max_overflow, scopefunc)
 
-    def connect(self):
-        if self.db_uri and self.db_uri.startswith(f"{self.db_type}://"):
-            self.engine = create_engine(self.db_uri, client_encoding="utf8", echo=self.debug,
-                                        poolclass=QueuePool, pool_size=self.pool_size,
-                                        max_overflow=self.max_overflow)
+    def connect(self, db_uri: str, db_type: str, debug: bool,
+                pool_size: int, max_overflow: int, scopefunc):
+        if db_uri and db_uri.startswith(f"{db_type}://"):
+            self.engine = create_engine(db_uri, client_encoding="utf8", echo=debug,
+                                        poolclass=QueuePool, pool_size=pool_size,
+                                        max_overflow=max_overflow)
         else:
-            raise ValueError(f"Invalid database URI or type. Must start with '{self.db_type}://'.")
+            raise ValueError(f"Invalid database URI or type. Must start with '{db_type}://'.")
 
-        self.base.metadata.bind = self.engine
-        self.base.metadata.create_all(self.engine)
-        self.session = scoped_session(sessionmaker(bind=self.engine, autoflush=False, scopefunc=self.scopefunc))
+        self.metadata.bind = self.engine
+        self.Base.metadata.create_all(self.engine)
+        self.Session = scoped_session(sessionmaker(bind=self.engine, autoflush=False, scopefunc=scopefunc))
 
     def close(self):
-        if self.session:
-            self.session.close_all()
+        if self.Session:
+            self.Session.close_all()
 
     def get_session(self) -> scoped_session:
-        return self.session
+        return self.Session
 
     def get_base(self):
-        return self.base
+        return self.Base
 
     def get_engine(self):
         return self.engine
