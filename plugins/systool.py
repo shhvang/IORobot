a9947f4818f2adbde5d5fbd350b82a7d0632cc55
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-23 18:47:34 +0530

    Update systool.py

--- plugins/systool.py
+++ plugins/systool.py
@@ -5,18 +5,8 @@ from telegram.constants import ParseMode
 
 from plugins import tlang
 from Main.utils.decorators import kiyocmd
+from Main.utils.tools import get_readable_time
 from Main import start_time
-
-def get_readable_time(seconds: int) -> str:
-    intervals = [(' days, ', 86400), ('h:', 3600), ('m:', 60), ('s', 1)]
-    time_string = ''
-    for name, count in intervals:
-        value = seconds // count
-        if value > 0:
-            seconds -= value * count
-            time_string += f'{value}{name}'
-
-    return time_string
 
 @kiyocmd('start')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
