0e3a1de2eba8f651b8e113926269e08298269c79
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-23 18:49:21 +0530

    Update systool.py

--- plugins/systool.py
+++ plugins/systool.py
@@ -10,16 +10,18 @@ from Main import start_time
 
 @kiyocmd('start')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.type == "private":
-        return await update.effective_message.reply_text(
-            tlang(update.effective_chat.id, 'private_start').format(
+    chat = update.effective_chat
+    message = update.effective_message
+    if chat.type == "private":
+        return await message.reply_text(
+            tlang(chat.id, 'private_start').format(
                update.effective_user.first_name, context.bot.first_name 
             ),
             parse_mode=ParseMode.MARKDOWN,
         )
     else:
-        await update.effective_message.reply_text(
-            tlang(update.effective_chat.id, 'start_string').format(
+        await message.reply_text(
+            tlang(chat.id, 'start_string').format(
                 context.bot.first_name, get_readable_time(int(time.time() - start_time))
             ),
             parse_mode=ParseMode.MARKDOWN,
