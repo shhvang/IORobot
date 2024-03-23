b6ab821be6120af98dd3b306dc7eee20cd53690a
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-23 17:12:35 +0530

    Update systool.py

--- plugins/systool.py
+++ plugins/systool.py
@@ -21,7 +21,12 @@ def get_readable_time(seconds: int) -> str:
 @kiyocmd('start')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.type == "private":
-        pass
+        await update.effective_message.reply_text(
+            tlang(update.effective_chat.id, 'private_start').format(
+                update.effective_user.first_name, context.bot.first_name 
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+        )
     else:
         await update.effective_message.reply_text(
             tlang(update.effective_chat.id, 'start_string').format(
