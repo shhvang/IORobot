a2e4ec056ad63b0e778ac3bdec483461c55d079f
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-23 18:12:24 +0530

    Create alive.py

---
+++ plugins/alive.py
@@ -0,0 +1,21 @@
+from plugins.systool import get_readable_time
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
+
+from plugins import tlang
+from Main.utils.decorators import kiyocmd
+from Main import start_time
+
+alive_button = [ nvm
+
+]
+
+@kiyocmd('alive')
+async def alive(update: Update, context: ContextTypes.DEFAULTYPE):
+    await update.effective_message.reply_text(
+        tlang(update.effective_chat.id, 'alive_string').format(
+            context.bot.first_name, get_readable_time(int(time.time() - start_time))
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+        )
