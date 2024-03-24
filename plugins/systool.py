3f74147d001dbe822446809c62a3b931dd6cb993
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-24 16:15:13 +0530

    Update systool.py

--- plugins/systool.py
+++ plugins/systool.py
@@ -1,11 +1,11 @@
 import time
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
 
 from plugins import tlang
 from Main.utils.decorators import kiyocmd
-from Main.utils.tools import get_readable_time
+from Main.utils.string_handler import get_readable_time
 from Main import start_time
 
 @kiyocmd('start')
@@ -13,10 +13,39 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     message = update.effective_message
     if chat.type == "private":
-        return await message.reply_text(
-            tlang(chat.id, 'private_start').format(
-               update.effective_user.first_name, context.bot.first_name 
+        return await message.reply_video(
+            video='https://i.imgur.com/wf7DcAh.mp4',
+            caption=tlang(chat.id, 'private_start').format(
+               update.effective_user.first_name, context.bot.first_name, 
+               get_readable_time(int(time.time() - start_time))
             ),
+            reply_markup=InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton(
+                        f'{(tlang(chat.id, 'support_btn'))}', 
+                        url='https://t.me/AyakaSupport'
+                    ),
+                    InlineKeyboardButton(
+                        f'{(tlang(chat.id, 'update_btn'))}', 
+                        url='https://t.me/AyakaUpdates'
+                    ),
+                    InlineKeyboardButton(
+                        f'{((tlang(chat.id, 'source_btn')))}', 
+                        url='https://github.com/iOpacity/rKiyoBot'
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        f'{(tlang(chat.id, 'inline_btn'))}', 
+                        switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        f'{(tlang(chat.id, 'add_btn'))}', 
+                        url='https://t.me/ayaka_robot?startgroup=true'
+                    )
+                ]
+            ]
+        ),
             parse_mode=ParseMode.MARKDOWN,
         )
     else:
