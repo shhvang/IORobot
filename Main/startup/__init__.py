720cde28a734fdd94db3648d04fdd65c95cfddbe
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-25 18:41:20 +0530

    Update __init__.py

--- Main/startup/__init__.py
+++ Main/startup/__init__.py
@@ -3,26 +3,36 @@ from logging.handlers import RotatingFileHandler, MemoryHandler
 from logging import captureWarnings, basicConfig, StreamHandler, WARNING, INFO, DEBUG, getLogger
 
 def Logger():
+    path = pathlib.Path('./Main/logger') / 'kiyo.log'
+    path.parent.mkdir(parents=True, exist_ok=True)
+
     _LOGS = getLogger('Kiyo')
-    log_path = pathlib.Path('./Main/logger/') / f'kiyo.log'
-    log_path.parent.mkdir(parents=True, exist_ok=True)
+    _LOG_FMT = '%(asctime)s | %(name)s [%(levelname)s] : %(messagse)s'
+
+    getLogger('sqlalchemy.engine').setLevel(WARNING)
+    getLogger('psycopg2').setLevel(WARNING)
+    getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(WARNING)
+    getLogger('apscheduler.scheduler').setLevel(INFO)
+    getLogger("httpx").setLevel(WARNING)
     captureWarnings(True)
-    basicConfig(format='[%(asctime)s] - [IOpacity] << %(levelname)s >> %(name)s - %(message)s',
+    
+    
+    basicConfig(format=_LOG_FMT,
                 datefmt='%d-%b-%y %H:%M:%S',
                 handlers=[
                     RotatingFileHandler(
-                        log_path, maxBytes=1024 * 1024, backupCount=5, encoding='utf-8'
+                        path, maxBytes=1024 * 1024, backupCount=5, encoding='utf-8'
                     ),
                     MemoryHandler(1024 * 1024, WARNING),
                         StreamHandler(),
                     ],
                     level=INFO,
                 )
-    
-    getLogger('sqlalchemy.engine').setLevel(WARNING)
-    getLogger('psycopg2').setLevel(WARNING)
-    getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(WARNING)
-    getLogger('apscheduler.scheduler').setLevel(INFO)
-    getLogger("httpx").setLevel(WARNING)
+    try:
+        import coloredlogs
+
+        coloredlogs.install(level=None, logger=_LOGS, fmt=_LOG_FMT)
+    except ImportError:
+        pass
     
     _LOGS.info('Initialized Logger!')
