b4224c90c8745d4e02cfe6f64286fc6a37e200b5
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-25 09:16:07 +0530

    Update rules.py

--- plugins/rules.py
+++ plugins/rules.py
@@ -9,7 +9,7 @@ from telegram import (
     User,
 )
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, filters
+from telegram.ext import CallbackContext, filters, CommandHandler
 from telegram.helpers import escape_markdown
 from telegram.constants import ParseMode
 from Main.utils.string_handler import markdown_parser
@@ -18,9 +18,10 @@ from plugins import tlang
 from Main import application
 from Main.utils.decorators import kiyocmd, rate_limit
 
+async def get_rules(update: Update, _: CallbackContext):
+    chat_id = update.effective_chat.id
+    await send_rules(update, chat_id)
 
-@kiyocmd('rules')
-@rate_limit(40, 60)
 async def send_rules(update: Update, chat_id, from_pm=False):
     bot = application.updater.bot
     message = update.effective_message
@@ -30,7 +31,7 @@ async def send_rules(update: Update, chat_id, from_pm=False):
         if excp.message != "Chat not found" or not from_pm:
             raise
 
-        bot.send_message(
+        await bot.send_message(
             update.effective_user.id,
             "The rules shortcut for this chat hasn't been set properly! Ask admins to "
             "fix this.\nMaybe they forgot the hyphen in ID",
@@ -40,11 +41,11 @@ async def send_rules(update: Update, chat_id, from_pm=False):
     text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
 
     if from_pm and rules:
-        bot.send_message(
+        await bot.send_message(
             update.effective_user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
     elif from_pm:
-        bot.send_message(
+        await bot.send_message(
             update.effective_user.id,
             "The group admins haven't set any rules for this chat yet. "
             "This probably doesn't mean it's lawless though...!",
@@ -61,17 +62,17 @@ async def send_rules(update: Update, chat_id, from_pm=False):
         )
         txt = "Please click the button below to see the rules."
         if not message.reply_to_message:
-            message.reply_text(txt, reply_markup=btn)
+            await message.reply_text(txt, reply_markup=btn)
 
         if message.reply_to_message:
-            message.reply_to_message.reply_text(txt, reply_markup=btn)
+            await message.reply_to_message.reply_text(txt, reply_markup=btn)
     else:
-        update.effective_message.reply_text(
+        await update.effective_message.reply_text(
             "The group admins haven't set any rules for this chat yet. "
             "This probably doesn't mean it's lawless though...!"
         )
 
-@kiyocmd('rules')
+@kiyocmd('setrules')
 @rate_limit(40, 60)
 async def set_rules(update: Update, context: CallbackContext):
   chat_id = update.effective_chat.id
@@ -87,3 +88,5 @@ async def set_rules(update: Update, context: CallbackContext):
 
       sql.set_rules(chat_id, markdown_rules)
       await update.effective_message.reply_text("Successfully set rules for this group.")
+
+application.add_handler(CommandHandler(get_rules, 'rules', filters=filters.ChatType.GROUPS))
