fe53f9a60e55b4e24847d4034cb9255f4257a67a
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-26 10:00:27 +0530

    Update __main__.py

--- Main/__main__.py
+++ Main/__main__.py
@@ -11,16 +11,9 @@ from . import (
 from Main.langs import setup_localization
 from Main.startup.helpers import load_all_modules
 
-async def error_handler(update, context):
-    """Log the error."""
-    LOGS.error(msg="Exception while handling an update:", exc_info=context.error)
-
-
 def main():
     setup_localization()
     load_all_modules()
-    LOGS.info('[KIYO] Started Using long polling.')
-    application.add_error_handler(error_handler)
     application.run_polling()
 
 if __name__ == '__main__':
