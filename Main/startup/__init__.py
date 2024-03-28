85e0101dc010f3987ee7252c8e3d1e8727d8105f
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-28 20:24:37 +0530

    Update __init__.py

--- Main/startup/__init__.py
+++ Main/startup/__init__.py
@@ -1,37 +1,4 @@
 import pathlib, os, configparser
-from logging.handlers import RotatingFileHandler, MemoryHandler
-from logging import captureWarnings, basicConfig, StreamHandler, WARNING, INFO, DEBUG, getLogger
-from colorama import init, Fore, Style
-
-def Logger():
-    path = pathlib.Path('./Main/logger') / 'kiyo.log'
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    _LOGS = getLogger(__name__)
-    _LOG_FMT = f'{Fore.GREEN}%(asctime)s{Style.RESET_ALL} | {Fore.CYAN}%(name)s{Style.RESET_ALL} [{Fore.YELLOW}%(levelname)s{Style.RESET_ALL}] : %(message)s'
-
-    getLogger('sqlalchemy.engine').setLevel(WARNING)
-    getLogger('psycopg2').setLevel(WARNING)
-    getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(WARNING)
-    getLogger('apscheduler.scheduler').setLevel(INFO)
-    getLogger("httpx").setLevel(WARNING)
-    captureWarnings(True)
-    
-    
-    basicConfig(format=_LOG_FMT,
-                datefmt='%d-%b-%y %H:%M:%S',
-                handlers=[
-                    RotatingFileHandler(
-                        path, maxBytes=1024 * 1024, backupCount=5, encoding='utf-8'
-                    ),
-                    MemoryHandler(1024 * 1024, WARNING),
-                        StreamHandler(),
-                    ],
-                    level=INFO,
-                )
-    
-    init(autoreset=True)
-    _LOGS.info('Initialized Logger!')
 
 def isLocalHost():
     return os.path.exists("./localhost.txt")
