440f5c8e6640ab96d88485f9748f0e4933d65d25
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-04-01 15:20:04 +0530

    Create start_helpers.py

---
+++ kiyo_plugins/utils/start_helpers.py
@@ -0,0 +1,33 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from yuii_plugins.external.langhelpers import tlang
+
+def gen_start_kb(chat=None):
+    if chat:
+        keyboard =  [
+                        [
+                            InlineKeyboardButton(
+                                text=f"{tlang(chat.id, 'support_btn')}", 
+                                url='https://t.me/IOSupportGroup'
+                            ),
+                            InlineKeyboardButton(
+                                text=f"{tlang(chat.id, 'update_btn')}", 
+                                url='https://t.me/IOUpdate'
+                            ),
+                            InlineKeyboardButton(
+                                text=f"{tlang(chat.id, 'source_btn')}", 
+                                url='https://github.com/iOpacity/rKiyoBot'
+                            )
+                        ],
+                        [
+                            InlineKeyboardButton(
+                                text=f"{tlang(chat.id, 'inline_btn')}", 
+                                switch_inline_query_current_chat=""
+                            ),
+                            InlineKeyboardButton(
+                                text=f"{tlang(chat.id, 'add_btn')}", 
+                                url='https://t.me/rKiyoBot?startgroup=true'
+                            )
+                        ]
+                    ]
+                
+        return keyboard
