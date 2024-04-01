af06b6fc339742f977a3ca58be5bbf4dc652a227
Shivang <132191347+shhvang@users.noreply.github.com>
2024-04-01 18:34:30 +0000

    Add support for sending boot message with photo

--- IO/__main__.py
+++ IO/__main__.py
@@ -1,9 +1,24 @@
-from IO import kiyo
+from IO import kiyo, LOGGER
+from telegram import ParseMode
+from telegram.error import BadRequest, Unauthorized
 
-async def send_boot_message():
-    support_chat_id = -1002146661683  # Support Chat ID
-    boot_message = "Booting was succesfull!/nKiyo is alive again"
-    await kiyo.client.send_message(support_chat_id, boot_message)
+SUPPORT_CHAT = -1002146661683
+
+def send_boot_message():
+    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
+            try:
+                kiyo.client.send_photo(
+                    chat_id=f"@{SUPPORT_CHAT}",
+                    photo="https://telegra.ph/file/26ef5a0b523c9a52977ad.jpg",
+                    caption=f"⚡ Successfully Rebooted",
+                    parse_mode=ParseMode.MARKDOWN,
+                )
+            except Unauthorized:
+                LOGGER.warning(
+                    f"Bot is unable to send message to @{SUPPORT_CHAT}, go and check!"
+                )
+            except BadRequest as e:
+                LOGGER.warning(e.message)
 
 if __name__ == "__main__":
     kiyo.run()
