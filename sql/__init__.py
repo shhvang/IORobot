68d45eafb0fcb3fd2037993829100845e05d14db
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-04-01 15:21:29 +0530

    Update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -4,3 +4,6 @@ from kiyo import configs as config
 database = Database(
     config.DATABASE_URI, db_type='postgresql', pool_size=10, max_overflow=20, debug=False
 )
+Session = database.get_session()
+Engine = database.get_engine()
+Base = database.get_base()
