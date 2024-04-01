126c6d9f91cb6fface4121060ab41205b7a634cf
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2024-04-01 15:20:59 +0530

    Update start.py

--- kiyo_plugins/internal/bot/start.py
+++ kiyo_plugins/internal/bot/start.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes, CommandHandler
 from telegram.constants import ParseMode
+from kiyo_plugins.utils.start_helpers import gen_start_kb
 
 from kiyo import kiyo
 
@@ -19,12 +20,22 @@ def get_readable_time(seconds: int) -> str:
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
-    await message.reply_text(
-        "Hi, I'm {}\nuptime: `{}`".format(
-            context.bot.first_name, get_readable_time(int(time.time() - kiyo.startTime))
-        ),
-        parse_mode=ParseMode.MARKDOWN,
-        )
+    chat = update.effective_chat
+    if chat.type != 'private':
+        return await message.reply_text(
+            "Hi, I'm {}\nuptime: `{}`".format(
+                context.bot.first_name, get_readable_time(int(time.time() - yuii.startTime))
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+            )
+    else:
+        await message.reply_text(
+            "Hi, {} I'm {}".format(
+                update.effective_user.first_name, context.bot.first_name
+            ),
+            reply_markup=InlineKeyboardMarkup(gen_start_kb(chat)),
+            parse_mode=ParseMode.MARKDOWN,
+            )
 
 async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
